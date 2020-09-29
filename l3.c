64eb8ccbfef4af8aaae2c69a2e2ca5be49dc370e
alinanasonova <47684560+alinanasonova@users.noreply.github.com>
2020-09-29 13:27:26 +0700

    call l2

--- l3.c
+++ l3.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,7 +15,9 @@ int main(int argc, char *argv[])
     int c = 0;
     
     char *opt = argv[1];
-    
+
+    //pid_t p = fork();
+
     if(opt[1] == '-')
 	opt[1] = 'h';
     if(strcmp(opt, "-hhelp") && (opt[2] != NULL)){
@@ -27,11 +32,21 @@ int main(int argc, char *argv[])
 	    printf("Авторы: Александр Шихалев\n\tАлина Насонова\n\tГригорий Голомидов\nВсе доступные аргументы:\n\t-c - Строка ввода команд для вызова проекта из лабораторной 2\n");
 	    printf("\t-p - Порождение процесса по его имени и возврат в строку команд после завершения дочернего процесса\n");
 	    printf("\nКраткое описание проекта:\n\tПроект позволяет работать с процессами в ОС Linux\n");
-	    printf("Примеры запуска:\n\t./l2 -m <путь к файлу> <пункт назначения файла\n");
-	    printf("\t./l2 -c <путь к файлу> <пункт назначениф файла>\n\t./l2 -d <путь к файлу>");
-	    printf("\n\t./l2 -s <директория>\n\t./l2 -l <директория>\n");
-	    printf("\t<Путь к файлу>/<пункт назначения файла> для -m, -c, -d может быть путем к файлу </home/admin/text.txt> или именем файла в текущей папке <text.txt>\n");
-	    printf("\t<Директория> для -s может быть путем </home/admin/>, текущей директорией <.>, файлом в текущей дир. <text.txt>, файлом в другой дир. </home/admin/text.txt>\n");
-        break;
+	    printf("Примеры запуска:\n\t./l3 -c <путь к файлу> <аргументы>\n");
+	    printf("\t\n\t");
+	    printf("\n\t\n\t\n");
+	    printf("\t\n");
+	    printf("\t\n");
+	    break;
+	case 'c':
+	    //pid_t p = fork();
+	    //if(p == 0){
+	    execl(argv[2], argv[2], argv[3], argv[4], argv[5]);
+	    //exit(127);
+	    //}
+	    //else{
+	    //waitpid(p, 0, 0);
+	    //}
+	    break;
     }
 }
